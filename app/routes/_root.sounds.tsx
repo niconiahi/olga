85fecc7932ce623d33c3202382f2ad45b2b1f54f
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-04-11 13:41:21 -0300

    feat: add initial implementation of playing sound

--- app/routes/_root.sounds.tsx
+++ app/routes/_root.sounds.tsx
@@ -1,19 +1,48 @@
 import { LoaderFunctionArgs } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+import { useEffect, useRef } from "react";
 import { getEnv } from "~/utils/env";
+import { error } from "~/utils/http";
 
 export async function loader({ context }: LoaderFunctionArgs) {
   const env = getEnv(context)
-  const sounds = await env.SOUNDS.list()
-  console.log('sounds', sounds)
-  const dudoso = await env.SOUNDS.get("dudoso.p3")
-  console.log('dudoso', dudoso)
-
-
-  return null
+  const sound = await env.SOUNDS.get("dudoso.mp3")
+  if (sound === null) {
+    return error(404, 'Not found');
+  }
+  return new Response(sound.body)
 }
 
 export default function() {
+  const loaderData = useLoaderData<typeof loader>()
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  useEffect(() => {
+    async function createObjectUrl() {
+      console.log()
+      const response = new Response(loaderData as ReadableStream)
+      const blob = await response.blob()
+      const url = URL.createObjectURL(blob)
+      return url
+    }
+
+    createObjectUrl().then((objectUrl) => {
+      console.log("objectUrl", objectUrl)
+      const audioElement = audioRef.current
+      if (audioElement) {
+        audioElement.src = objectUrl
+        audioElement.addEventListener("canplaythrough", () => {
+          audioElement.play();
+        });
+      }
+    })
+
+  }, [loaderData])
+
   return (
-    <h1>sounds</h1>
+    <figure>
+      <figcaption>Listen to the T-Rex:</figcaption>
+      <audio ref={audioRef} controls src="."></audio>
+    </figure>
   )
 }
