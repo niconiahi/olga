e85caf0d46e1448e7baa55a0c306d680dbcf8d48
Nicolas Accetta <nicolas.accetta@gmail.com>
2024-06-11 18:10:37 -0300

    feat: add plausible

--- app/root.tsx
+++ app/root.tsx
@@ -22,6 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script defer data-domain="olga.media" src="https://plausible.io/js/script.js"></script>
         <Meta />
         <Links />
       </head>
